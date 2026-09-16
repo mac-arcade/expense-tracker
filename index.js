4f945f1d92119295f17a00686e93bf8b1fd3dece
mac-arcade <arcade2903@gmail.com>
2026-09-16 16:25:03 +0530

    feat: add custom request logger middleware

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
+// Import express
 const express = require("express");
 
+// Create express application
 const app = express();
 
+// Give PORT number
 const port = process.env.port || 3000;
 
+
+const logger = (req, res, next) => {
+
+    const time = new Date().toLocaleTimeString();
+
+    console.log(`[${time}] ${req.method} ${req.url}`);
+
+    next();
+};
+
+// Middleware to parse json
 app.use(express.json());
 
+// Custom middleware to log 
+app.use(logger);
+
+// Default home route
 app.get("/", (req, res) => {
     res.status(200).send("Expense Tracker API");
 });
 
+// Start app
 app.listen(port, () => {
     console.log(`Server is started at http://localhost:${port}`);
 })
